5f95180c3d3cb5db88031ab8477de799964fb234
CarneMoida137 <91175857+CarneMoida137@users.noreply.github.com>
2022-09-27 00:55:25 -0300

    Add files via upload

---
+++ exercio4RECURSIVIDADE.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <conio.h>
+int expo(int x, int y)
+{
+ if (y == 0)
+ return 1;
+ if (y == 1)
+ return x;
+ return x*expo(x,y
+-1);
+}
+
+int main
+(void) {
+int x, y, e;
+ printf("Exponencial de x elevado a y\n\n");
+ printf("Digite o numero inteiro x:");
+ scanf("%d", &x);
+ printf("Digite o numero inteiro y:");
+ scanf("%d", &y);
+ if (y < 0) {
+ printf("y tem que ser maior ou igual a zero!!");
+ getch(); }
+ else
+{
+ e=expo(x,y);
+ printf("\n\nX elevado a y eh: %d", e);
+ getch();
+ }
+}
